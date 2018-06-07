a45843b375268e9791efb5a00f644793f4280fb9
jrcunning <ross.cunning@gmail.com>
2018-06-07 12:44:46 -0400

    change queue and fps

--- annotations/adig_ips.sh
+++ annotations/adig_ips.sh
@@ -2,16 +2,16 @@
 #BSUB -J adig_ips
 #BSUB -o %J.out
 #BSUB -e %J.err
-#BSUB -q parallel
+#BSUB -q general
 #BSUB -P crf
-#BSUB -n 16
+#BSUB -W 48:00
 #
 
 module load java/1.8.0_60
 
 export _JAVA_OPTIONS=''
 
-cd /scratch/projects/crf/acerv-gwas/annotation
-interproscan.sh -i GCF_000222465.1_Adig_1.1_protein.faa -b adig_ips --goterms
+cd /scratch/projects/crf/acerv-gwas/annotations
+interproscan.sh -i adig_proteins.faa -b adig_ips --goterms
 
 sed -n 's/\(aug_v2a.........\).*\(GO:.*\)/\1 \2/p' adig_ips.tsv | sort | uniq | awk -F' ' -v OFS=', ' '{x=$1;$1="";a[x]=a[x]$0}END{for(x in a)print x,a[x]}' | sed 's/, , /\t/g' > adig_ips_GO.txt
