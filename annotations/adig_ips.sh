45a9f5887ad2e98518d0360c500231b36c70ef4a
Ross Cunning <ross.cunning@gmail.com>
2018-06-07 12:29:54 -0400

    add script and ips job

---
+++ annotations/adig_ips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -J adig_ips
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q parallel
+#BSUB -P crf
+#BSUB -n 16
+#
+
+module load java/1.8.0_60
+
+export _JAVA_OPTIONS=''
+
+cd /scratch/projects/crf/acerv-gwas/annotation
+interproscan.sh -i GCF_000222465.1_Adig_1.1_protein.faa -b adig_ips --goterms
+
+sed -n 's/\(aug_v2a.........\).*\(GO:.*\)/\1 \2/p' adig_ips.tsv | sort | uniq | awk -F' ' -v OFS=', ' '{x=$1;$1="";a[x]=a[x]$0}END{for(x in a)print x,a[x]}' | sed 's/, , /\t/g' > adig_ips_GO.txt
