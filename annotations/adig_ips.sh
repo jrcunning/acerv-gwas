7ecffd73a33d26ce90cd4fb20e853805445f6fe3
Ross Cunning <ross.cunning@gmail.com>
2018-06-12 08:09:21 -0400

    clean up

--- annotations/adig_ips.sh
+++ annotations/adig_ips.sh
@@ -15,5 +15,3 @@ export _JAVA_OPTIONS=''
 cd /scratch/projects/crf/acerv-gwas/annotations
 
 interproscan.sh -i adig_proteins.faa -dp --appl TIGRFAM,SFLD,ProDom,Hamap,SMART,CDD,ProSiteProfiles,ProSitePatterns,SUPERFAMILY,PRINTS,PANTHER,Gene3D,PIRSF,Pfam,Coils --goterms -b adig_ips_p -f tsv
-
-#sed -n 's/\(aug_v2a.........\).*\(GO:.*\)/\1 \2/p' adig_ips.tsv | sort | uniq | awk -F' ' -v OFS=', ' '{x=$1;$1="";a[x]=a[x]$0}END{for(x in a)print x,a[x]}' | sed 's/, , /\t/g' > adig_ips_GO.txt
